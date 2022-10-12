b836bd59947bf4571defc87f10d1c81cf7788780
Dean Beckingham <retroownerxd@gmail.com>
2022-10-12 11:55:32 +0100

    beginning javascript code and general tweaks

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+window.addEventListener("load", function() {
+
+    const form = document.querySelector("#new-task-form");
+    const input = document.querySelector("#new-task-input");
+    const list_el = document.querySelector("#tasks");
+
+    //this links all three together to load upon launching the webpage.
+
+    form.addEventListener("submit", function(e){
+        e.preventDefault();
+    })
+
+
+
+});
+
+
 
